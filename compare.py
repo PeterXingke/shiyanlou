e33ebaab221576e524e27e92f2be6ad2c7206598
xingke <383732383@qq.com>
2022-05-11 18:37:34 +0800

    new file

---
+++ compare.py
@@ -0,0 +1,15 @@
+a = int(input("Please enter a number:"))
+
+if a > 10:
+	print('a> 10')
+elif a == 10:
+	print('a == 10')
+else:
+	print('a < 10')
+
+if a<1:
+	print("a<1")
+else:
+	pass
+#程序没有报错
+
